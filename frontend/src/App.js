2542c38081f4adcd211263ea0f4ff7a089021bbe
Jigar <shahjigar185@gmail.com>
2021-09-15 17:48:43 +0530

    Added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Signup from "./components/signup"
+import Login from './components/login'
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Signup />
+      <Route path='/signup' component={Signup}/>
+      <Route path='/login' component={Login} />
     </div>
   );
 }
